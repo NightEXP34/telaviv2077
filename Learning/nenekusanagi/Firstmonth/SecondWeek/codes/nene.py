6e1f0b43a7367aac12392f132c92dcc4b79a026a
zakoding <afmuzakki233@gmail.com>
2026-06-17 00:45:32 +0700

    Create nene.py

---
+++ Learning/nenekusanagi/Firstmonth/SecondWeek/codes/nene.py
@@ -0,0 +1,23 @@
+#okay im re-learning py today ig since i probably forgot lots of it already
+
+print("nene kusanagi addict") #its gonna print the word "nene kusanagi addict surely
+
+#well like normal theres math surely
+print(2 + 3) #addiction/penambahan(+)
+print(2 - 3) #subtraction/pengurangan(-)
+print(2 * 3) #multiplication/perkalian(*)
+print(3 / 2) #division/pembagian(/)
+print(3 ** 2) #exponential/eksponensial (**)
+print(3 % 2) #modulus (%)
+print(3 // 2) #floor division operator (//) (if you didnt know its just division but
+              #its always gonna be round up to the lower number for example 7 / 3 = 2.33
+              #but in division its 2 and if the number for example 7.6 its still gonna go lower to 7
+
+#checking data types
+print(type(23))                  # Int
+print(type(2.23))                # Float
+print(type(2 + 3j))              # Complex
+print(type('nene'))          # String
+print(type([3, 2, 3]))           # List
+print(type({'name': 'nenekusanagi'}))  # Dictionary
+print(type({2.3, 3.23, 2.333}))    # Tuple
